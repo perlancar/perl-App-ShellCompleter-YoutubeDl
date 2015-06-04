b3e799bf817a1d27d2bf512374929a3186b72e77
perlancar (@pc-office) <perlancar@gmail.com>
2015-06-04 12:17:34 +0700

    Initial commit

---
+++ lib/App/ShellCompleter/YoutubeDl.pm
@@ -0,0 +1,16 @@
+package App::ShellCompleter::YoutubeDl;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Shell completion for youtube-dl
+
+=head1 SYNOPSIS
+
+See L<_youtube-dl> included in this distribution.
+
+
+=head1 SEE ALSO
+
+youtube-dl, L<https://rg3.github.io/youtube-dl/>
